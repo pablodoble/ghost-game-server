bc9fcf6fc44c2ec42e0412d9d8788e051306e75e
pablodoble <marquezdoble@gmail.com>
2017-03-07 23:32:59 +0100

    Included body-parser in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var bodyParser = require("body-parser");
 
 // Config
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Root
